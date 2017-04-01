c2b124849b7f139ec87add6dbda0f3c1fecb14f3
Raimon Grau <raimonster@gmail.com>
2017-04-02 01:11:43 +0200

    Better bash formatting.

--- bin/get-quotes.sh
+++ bin/get-quotes.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
 curl https://raw.githubusercontent.com/kulinp/quote-of-the-day/master/app/src/main/assets/txt/randomquote.txt >quotes.txt
-curl http://worrydream.com/quotes/ | pup 'div.entry json{}' |jq -r '.[]| [.children[1].children[0].children[0].text , .children[0].children[1].text ] | join(" -- ") | gsub("\\n" ; "")' >>quotes.txt
-curl http://skeeto.s3.amazonaws.com/share/showerthoughts | sed -e 's/^%$/%%%/'| tr '\r\n' '  ' | sed -s "s/ %%% /\n/g" >>quotes.txt
+curl http://worrydream.com/quotes/ \
+    | pup 'div.entry json{}' \
+    |jq -r '.[]| [.children[1].children[0].children[0].text , .children[0].children[1].text ] | join(" -- ") | gsub("\\n" ; "")' \
+        >>quotes.txt
+curl http://skeeto.s3.amazonaws.com/share/showerthoughts \
+    | sed -e 's/^%$/%%%/' \
+    | tr '\r\n' '  '\
+    | sed -s "s/ %%% /\n/g" \
+          >>quotes.txt
