ce4895422450cf6aea483468363e952913c75c4b
Raimon Grau <raimonster@gmail.com>
2017-04-02 01:09:46 +0200

    Format fixes.

--- bin/get-quotes.sh
+++ bin/get-quotes.sh
@@ -2,4 +2,4 @@
 
 curl https://raw.githubusercontent.com/kulinp/quote-of-the-day/master/app/src/main/assets/txt/randomquote.txt >quotes.txt
 curl http://worrydream.com/quotes/ | pup 'div.entry json{}' |jq -r '.[]| [.children[1].children[0].children[0].text , .children[0].children[1].text ] | join(" -- ") | gsub("\\n" ; "")' >>quotes.txt
-curl http://skeeto.s3.amazonaws.com/share/showerthoughts | sed -e 's/^%$/%%%/'| tr -d '\r\n' | sed -s "s/%%%/\n/g" >>quotes.txt
+curl http://skeeto.s3.amazonaws.com/share/showerthoughts | sed -e 's/^%$/%%%/'| tr '\r\n' '  ' | sed -s "s/ %%% /\n/g" >>quotes.txt
